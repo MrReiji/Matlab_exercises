2fd0a5313bc40f93317353551098770a021ba94d
Gabriel Rosół <64375466+MrReiji@users.noreply.github.com>
2022-10-11 17:44:27 +0200

    Matlab - laboratory 2

---
+++ problem2_1.m
@@ -0,0 +1,13 @@
+% Lab 2 Problem 2.1
+
+clear all;
+
+a = fi( 141,0,8,0), a.bin,
+b = fi( 115,0,8,0), b.bin,
+c = fi( 115,1,8,0), c.bin,
+d = fi(-115,1,8,0), d.bin,
+
+u1 = fi(22,0,8,0), u1.bin,
+u2 = fi(22,1,8,0), u2.bin,
+u3 = fi(22,0,16,0), u3.bin,
+u4 = fi(22,1,16,0), u4.bin,
